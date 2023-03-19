4a27a8869e1ea2e86ada7aad5b129ae76fb869cd
oliviafromnz <100629335+oliviafromnz@users.noreply.github.com>
2023-03-19 15:55:10 +0100

    Ceilig colour change

--- example_frontscroll.js
+++ example_frontscroll.js
@@ -2,15 +2,17 @@ function draw_one_frame(cur_frac) {
   // VARIABLES
 
   let window_size = canvasHeight/2; // window size
-  
-  // COLOURS
+
+
+  /////////////// COLOURS //////////////
+
   let window_color1 = color("#8AB0BF") // light blue colour
   let window_color2 = color("#D0EEF7") // lighter blue colour
   let backgroundColor = color ("#D9B384") // background colour sand
   let framingColor = color ("#A65F21") //framing colour 
   let celingColor = color ("#EBE3AB") //ceiling colour light sand
   let darkeygreen = color ("#93AEBF")  // dark greeny blue
-  //let peachycolour = color("#F2B885") //peachycolour
+  let framingdetail = color ("#7d4718")
   let windowXsize = width/2;
   let windowYsize = height*1;
 
@@ -18,40 +20,27 @@ function draw_one_frame(cur_frac) {
   let backupColor = color("#C19A36") // purple
   let detailColor = color ("#66CAD8") //yellow
 
-///////////////// VARIABLES
+///////////////// CEILING //////////////
 
-// let orbSize = width / 20
-// let spacingSize = width / 35
-
-let noiseColor = getNoiseValue(0, 0, 0.8, "noiseColor", 0, 1, 1)
-let noiseyColor;
-
-////////////////// MAPS
-
-let moveXmap, moveYMap;
-  
   noStroke();
-
-  ////// background
   fill(celingColor);
   rect(0, 0, width, height);
 
+  
+// /// array for ceiling arc colour ////
 
-///////////// arcs repeating on ceiling colour series
-  let arcColor = [
-    color("#A6785D"),
-    color("#F2B885"),
-    color("#A65D03"),
-    color("#F2C46D"),
-    color("#93AEBF"),
-    color("#93AEBF") // dark greeny blue
-  ]
+//   let arcColor = [
+//     color("#A6785D"),
+//     color("#F2B885"),
+//     color("#A65D03"),
+//     color("#F2C46D"),
+//     color("#93AEBF"),
+//     color("#f0c38d")
+//   ]
 
-///////////// where ellipses spawn
+/// where ellipses spawn //////
     strokeWeight(1);
     let circ_points = [
-      -0.5 * height,
-      0.5 * height,
       0.75 * height,
       1 * height,
       1.25 * height,
@@ -63,52 +52,105 @@ let moveXmap, moveYMap;
       2.75 * height,
     ]
 
-///////////// making the arcs continue to spawn and move upwards 
-    strokeWeight(1);
-stroke(0);
+let colors = [
+  color("#A6785D"),  // red
+  color("#F2B885"),  // green
+  color("#93AEBF"),  // blue
+];
+
+push()
 for (let i = 0; i < circ_points.length - 1; i++) {
-  let cur_grid_line = map(cur_frac, 1, 0, circ_points[i], circ_points[i + 1]);
-  let value = map(cur_grid_line, 0, height, 0, 1);
-  let arcColor_change = lerpColor(arcColor[0], arcColor[3], value);
-  fill(arcColor_change);
-  ellipse(width / 2, cur_grid_line, windowXsize * 2, windowYsize * 4.3);
-}
+  // Calculate the position of the ellipse
+  let y = map(cur_frac, 1, 0, circ_points[i], circ_points[i + 1]);
   
-  if (debugView) {
-      strokeWeight(1);
-      stroke(255, 0, 0);
-      for(let i=0; i<circ_points.length; i++) {
-        ellipse(width/2, circ_points[i], width, circ_points[i]);
-      }
-    } 
+  // Calculate the color based on the position
+  let t = map(y, circ_points[i], circ_points[i + 1], 0, 1);
+  let c = lerpColor(colors[0], colors[2], t);
+
+  // Draw the ellipse with the calculated color
+  fill(c);
+  strokeWeight(width/30)
+  stroke(framingColor)
+  ellipse(width / 2, y, windowXsize * 1.9, windowYsize * 4.4);
+} 
+pop()
+
+// ///// making the arcs spawn and move upwards ////
+//    push()
+// strokeWeight(1);
+// stroke(0);
+// for (let i = 0; i < circ_points.length - 1; i++) {
+//   let cur_grid_line = map(cur_frac, 1, 0, circ_points[i], circ_points[i + 1]);
+//   let value = map(cur_grid_line, -1, 1, 0, 1);
+//   let arcColor_change = lerpColor(arcColor[0], arcColor[5], value);
+//   fill(arcColor_change);
+//   ellipse(width / 2, cur_grid_line*1.04, windowXsize * 1.9, windowYsize * 4.4);
+
+//   //console.log('cur_grid_line:', cur_grid_line);
+//   //console.log('value:', value);
+// }
+  
+//   if (debugView) {
+//       strokeWeight(1);
+//       stroke(255, 0, 0);
+//       for(let i=0; i<circ_points.length; i++) {
+//         ellipse(width/2, circ_points[i], width, circ_points[i]);
+//       }
+//     } 
+
+// pop()
+
+  /////////// WALLS BASE //////////////
+
+  //left wall
+  fill(backgroundColor);
+  quad(width*0.35, height, windowXsize*0.25, height*0, windowXsize*0, height*0, width*0.001, height)
+
+  //right wall
+  quad(width*0.65, height, windowXsize*1.75, height*0, windowXsize*2, height*0, width, height)
 
 
-  //////////// window
+  ////////////// PAINTING BASES //////////////
+  push()
+  fill("#D9CDB8")
+  triangle(width*0.83, height*1.02, windowXsize*1.67, height*0.42, width*0.70, height*1.02) //right panel framing inner
+  triangle(width*0.17, height*1.02, windowXsize*0.33, height*0.42, width*0.30, height*1.02) //right panel framing inner
+  pop()
+
+  //////////// WINDOW //////////////
   
+  // window colour looping
+
   noStroke()
   let val = map(cur_frac, -1, 1, 0, 1)
   let window_change = lerpColor(window_color1, window_color2, val);
-  fill(window_change);
+  let window_changeback = lerpColor(window_color2, window_color1, val);
+  let window_colorloop = lerpColor(window_change, window_changeback, val);
+  fill(window_colorloop);
 
-  //fill(window_color); //light blue
+  // window main ellipse framing
 
   push()
   fill(framingColor);
-  ellipse(windowXsize, height*1.01, window_size*1.2);
+  ellipse(windowXsize, height*1.01, window_size*1.1);
   pop() 
+
+  // window main ellipse
 
   ellipse(windowXsize, height, window_size);
 
-  //window framing inner circ
+  //window framing inner circle
+
   push()
-  strokeWeight(7)
+  strokeWeight(width/100)
   stroke(framingColor)
   ellipse(windowXsize, height, window_size/1.20);
   pop()
 
   // window framining inner lines
+
   push()
-  strokeWeight(8)
+  strokeWeight(width/100)
   stroke(framingColor)
   line(width/2, height/1.26, width/2, height) // centre line 
   line(width/2.4, height/1.16, width/2, height) // left line
@@ -116,60 +158,51 @@ for (let i = 0; i < circ_points.length - 1; i++) {
   line(width/2.58, height, width/1.62, height) // bottom line
   pop()
 
+  ///////////// pixel effect on wall /////////////
 
-  /////////// walls base
-
-  //left wall
-  fill(backgroundColor);
-  quad(width*0.36, height, windowXsize*0.25, height*0, windowXsize*0, height*0, width*0.001, height)
-
-  //right wall
-  quad(width*0.64, height, windowXsize*1.75, height*0, windowXsize*2, height*0, width, height)
-  
-  // framing on walls
-  fill(framingColor);
-  // quad(width*0.12, height, windowXsize*0.25, height*0.17, windowXsize*0, height*-0.35, width*0, height) // left panel framing
-  // quad(width*0.88, height, windowXsize*1.75, height*0.17, windowXsize*2, height*-0.35, width, height) // right panel framing
-
-  triangle(width*0.32, height, windowXsize*0.30, height*0.27, windowXsize*0.30, height) //left panel framing inner
-  triangle(width*0.85, height, windowXsize*1.70, height*0.27, width*0.68, height) //right panel framing inner
-
-  push()
-  fill("#D9CDB8")
-  //scale()
-  triangle(width*0.83, height*1.02, windowXsize*1.67, height*0.42, width*0.70, height) //right panel framing inner
-  triangle(width*0.17, height*1.02, windowXsize*0.33, height*0.42, width*0.30, height) //right panel framing inner
-
-  pop()
-
-  ///////////// pictures on wall
-  let orbSize = width / 20;
-  let spacingSize = width / 37;
+  let orbSize = width / 40;
+  let spacingSize = width / 55;
   
   let shapePoints = []; // array to store the points defining the custom shape
   
+  // Defining quad and triangle points 
+
   fill("#D9CDB8");
   noStroke(30);
   beginShape(); // start defining the custom shape
-  shapePoints.push(createVector(width*0.90, height));
-  shapePoints.push(createVector(windowXsize*1.79, height*0.17));
+  shapePoints.push(createVector(width*0.91, height));
+  shapePoints.push(createVector(windowXsize*1.79, height*0.18));
   shapePoints.push(createVector(windowXsize*2, height*-0.25));
   shapePoints.push(createVector(width, height));
-  quad(width*0.90, height, windowXsize*1.79, height*0.17, windowXsize*2, height*-0.25, width, height); // right panel framing
-  shapePoints.push(createVector(width*0.10, height));
-  shapePoints.push(createVector(windowXsize*.21, height*0.17));
+  quad(width*0.90, height, windowXsize*1.79, height*0.17, windowXsize*2, height*-0.10, width, height); // right panel framing
+  
+  shapePoints.push(createVector(width*0.09, height));
+  shapePoints.push(createVector(windowXsize*.21, height*0.18));
   shapePoints.push(createVector(windowXsize*0, height*-0.25));
   shapePoints.push(createVector(width*0, height));
-  quad(width*0.10, height, windowXsize*.21, height*0.17, windowXsize*0, height*-0.25, width*0, height); // left panel framing
+  quad(width*0.10, height, windowXsize*.21, height*0.18, windowXsize*0, height*-0.10, width*0, height); // left panel framing
+  
+  shapePoints.push(createVector(width*0.84, height*1.02));
+  shapePoints.push(createVector(windowXsize*1.69, height*0.38));
+  shapePoints.push(createVector(width*0.71, height)); // left bottom point
+  triangle(width*0.83, height*1.02, windowXsize*1.69, height*0.38, width*0.70, height) //right panel framing inner
+  
+  shapePoints.push(createVector(width*0.17, height*1.02));
+  shapePoints.push(createVector(windowXsize*0.33, height*0.42));
+  shapePoints.push(createVector(width*0.29, height)); // right bottom point
+  triangle(width*0.17, height*1.02, windowXsize*0.33, height*0.42, width*0.30, height) //left panel framing inner
+
   endShape(CLOSE); // end the custom shape
   
-  // set of ellipses
-  for(let across = 1; across < width/spacingSize; across++) {
-    for(let down = 1; down < height/spacingSize; down++) {
+  // making loop for ellipses
+
+  for(let across = 0.5; across < width/spacingSize; across++) {
+    for(let down = 0.5; down < height/spacingSize; down++) {
       let x = spacingSize*across;
       let y = spacingSize*down;
       
-      // check if the ellipse is within the custom shape
+      // check if the ellipse is within the custom shape 
+
       if(isPointInShape(x, y, shapePoints)) {
         noiseColor = getNoiseValue(spacingSize*across, spacingSize*down, 0.9, "noiseColor", 0, 1, 20); // 200 is smooth value making it lumpy looking
       noiseyColor = lerpColor(mainColor, backupColor, noiseColor);
@@ -178,10 +211,12 @@ for (let i = 0; i < circ_points.length - 1; i++) {
         fill(finallerp);
         ellipse(x, y, orbSize);
       }
+
     }
   }
   
   // function to determine if a point is within a custom shape defined by an array of vertices
+  // (sourced from ChatGPT.com)
   function isPointInShape(x, y, vertices) {
     let inside = false;
     let j = vertices.length - 1;
@@ -196,12 +231,47 @@ for (let i = 0; i < circ_points.length - 1; i++) {
     return inside;
   }
   
+
+ ///////////////// FRAMING  ///////////////
+  
+ // framing for quads 
+
+ push()
+  noFill()
+  strokeWeight(width/50)
+  stroke(framingColor)
+  quad(width*0.90, height*1.02, windowXsize*1.79, height*0.17, windowXsize*2, height*-0.25, width, height*1.02); // right panel framing
+  quad(width*0.10, height*1.02, windowXsize*.21, height*0.17, windowXsize*0, height*-0.25, width*0, height*1.02); // left panel framing
+  pop()
+
+  // framing for triangle and wall ceiling mounting
+  
   push()
   noFill()
-  strokeWeight(20)
+  strokeWeight(width/50)
   stroke(framingColor)
-  quad(width*0.90, height, windowXsize*1.79, height*0.17, windowXsize*2, height*-0.25, width, height); // right panel framing
-  quad(width*0.10, height, windowXsize*.21, height*0.17, windowXsize*0, height*-0.25, width*0, height); // left panel framing
-pop()
+  triangle(width*0.17, height*1.02, windowXsize*0.33, height*0.42, width*0.30, height*1.02) //left panel framing inner
+  triangle(width*0.83, height*1.02, windowXsize*1.67, height*0.42, width*0.70, height*1.02) //right panel framing inner
+  line (width*0.65, height,windowXsize*1.75, height*0); // right wall
+  line (width*0.35, height,windowXsize*0.25, height*0); // left wall
+  pop()
+
+  // detailed framing for paintings and wall ceiling mounting
+
+  push()
+  noFill()
+  strokeWeight(width/100)
+  stroke(framingdetail)
+
+  line (width*0.65, height,windowXsize*1.75, height*0); // right wall
+  line (width*0.35, height,windowXsize*0.25, height*0); // left wall
+
+  triangle(width*0.17, height*1.02, windowXsize*0.33, height*0.42, width*0.30, height*1.02) //left panel framing inner
+  triangle(width*0.83, height*1.02, windowXsize*1.67, height*0.42, width*0.70, height*1.02) //right panel framing inner
+  
+  quad(width*0.90, height*1.02, windowXsize*1.79, height*0.17, windowXsize*2, height*-0.25, width, height*1.02); // right panel framing
+  quad(width*0.10, height*1.02, windowXsize*.21, height*0.17, windowXsize*0, height*-0.25, width*0, height*1.02); // left panel framing
+  pop()
+
   }   
   
